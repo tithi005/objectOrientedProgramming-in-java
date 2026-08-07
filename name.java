4cf9f6b9f56f2c5d3cb3faf428200bbae4ce4dec
Tithi Panda <pandatithi@gmail.com>
2026-08-07 23:01:19 +0530

    Taking name input through command line arguement

---
+++ name.java
@@ -0,0 +1,9 @@
+class name{
+    public static void main (String args[]){
+        if(args.length > 0){
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Enter your name as a command line argument");
+        }
+    }
+}
